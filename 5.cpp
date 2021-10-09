8172b50d2b24bea44516f3ecf6945557a74bec90
harshkumaragarwal <89272576+harshkumaragarwal@users.noreply.github.com>
2021-10-09 12:32:28 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+  char a='g';
+  bool b=1;
+  short c=5;
+  int d=6;
+  float e=1.9012;
+  double f=9.39384082;ffffe
+  long double g=4.4390283283241;
+  wchar_t h=L'A';
+  cout<<a<<" "<<sizeof(a)<<endl;
+  cout<<b<<" "<<sizeof(b)<<endl;
+  cout<<c<<" "<<sizeof(c)<<endl;
+  cout<<d<<" "<<sizeof(d)<<endl;
+  cout<<e<<" "<<sizeof(e)<<endl;
+  cout<<f<<" "<<sizeof(f)<<endl;
+  cout<<g<<" "<<sizeof(g)<<endl;
+  cout<<h<<" "<<sizeof(h)<<endl;
+
+}
